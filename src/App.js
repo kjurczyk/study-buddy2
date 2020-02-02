649f5c01569318901b15aeeb057c091716843635
Katarina Jurczyk <kjurczyk@ufl.edu>
2020-02-02 11:15:40 -0500

    started with animal kingdom

--- src/App.js
+++ src/App.js
@@ -1,40 +1,77 @@
 import React, { Component } from 'react'
 import logo from './uf_logo.png';
 import './App.css';
-//import { UserSession } from 'blockstack'
+import { UserSession } from 'blockstack'
 
+import Landing from './Landing'
+import SignedIn from './SignedIn'
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Welcome to Study Buddy!
-        </h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Login
-        </a>
-        <p>
-          or
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Register
-        </a>
-      </header>
-    </div>
-  );
+  constructor() {
+    super()
+    this.userSession = new UserSession()
+  }
+
+  componentWillMount() {
+    const session = this.userSession
+    if(!session.isUserSignedIn() && session.isSignInPending()) {
+      session.handlePendingSignIn()
+      .then((userData) => {
+        if(!userData.username) {
+          throw new Error('This app requires a username.')
+        }
+        window.location = `/kingdom/${userData.username}`
+      })
+    }
+  }
+
+  render() {
+    return (
+      <main role="main">
+          {this.userSession.isUserSignedIn() ?
+            <SignedIn />
+          :
+            <Landing />
+          }
+      </main>
+    );
+  }
 }
 
-export default App;
+export default App
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>
+//           Welcome to Study Buddy!
+//         </h1>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Login
+//         </a>
+//         <p>
+//           or
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Register
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
